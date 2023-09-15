d96c603be9ee1acec55bbc57707b4b3112c2cf42
AxelRaboit <axel.raboit@gmail.com>
2023-09-15 03:43:10 +0200

    fix

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,29 @@
-import { Header } from './Header';
-import { Navbar } from './Navbar';
-import { GlobalWrapper } from './GlobalWrapper';
-import { DefaultButton } from './Buttons';
-import { Home } from './Home';
-import { ArrowUp } from './ArrowUp';
-import { Profile } from './Profile';
-import { Skills } from './Skills/Skills';
-import { Projects } from './Projects/Projects';
-import { DynamicCv } from './DynamicCv';
-import { TestimonialsComp } from './Testimonials/Testimonials';
-import { TestimomnialsSlider } from './Testimonials/Slider';
-import { Footer } from './Footer';
+import { Header } from "./Header";
+import { Navbar } from "./Navbar";
+import { GlobalWrapper } from "./GlobalWrapper";
+import { DefaultButton } from "./Buttons";
+import { Home } from "./Home";
+import { ArrowUp } from "./ArrowUp";
+import { Profile } from "./Profile";
+import { Skills } from "./Skills/Skills";
+import { Projects } from "./Projects/Projects";
+import { DynamicCv } from "./DynamicCv";
+import { TestimonialsComp } from "./Testimonials/Testimonials";
+import { TestimomnialsSlider } from "./Testimonials/Slider";
+import { Footer } from "./Footer";
 
-export { Header, Navbar, GlobalWrapper, DefaultButton, Home, ArrowUp, Profile, Skills, Projects, DynamicCv, TestimonialsComp, TestimomnialsSlider, Footer }
+export {
+    Header,
+    Navbar,
+    GlobalWrapper,
+    DefaultButton,
+    Home,
+    ArrowUp,
+    Profile,
+    Skills,
+    Projects,
+    DynamicCv,
+    TestimonialsComp,
+    TestimomnialsSlider,
+    Footer
+};
